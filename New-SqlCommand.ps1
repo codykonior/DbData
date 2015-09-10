c18fd0905962b9eee5a68f8018b2d1dc07ffa1cb
codykonior <cody@codykonior.com>
2015-09-11 00:37:42 +0800

    new functions and placeholders

--- New-SqlCommand.ps1
+++ New-SqlCommand.ps1
@@ -6,6 +6,31 @@ Create an SQL Command object.
 .DESCRIPTION
 Create an SQL Command object. This includes combining a connection or connection string, with a query, parameter sets, timeouts, and transactions.
 
+.PARAMETER Query
+The query or stored procedure name to execute.
+
+.PARAMETER Parameters
+Zero or more parameters used in the query. These must be specified in the pattern of @VariableName and Value.
+
+.PARAMETER Connection
+Optional connection string, or SqlConnection object.
+
+.PARAMETER QueryTimeout
+Integer for the number of seconds to wait before expiring the query. The .NET default is 30 seconds.
+
+.PARAMETER Message
+The name of a globally scoped variable to hold any informational messages returned by SQL Server; for example through Print and Raiserror statements. If it does not exist it will be created if any of these are triggered.
+
+This normally holds Severity 10 and lower information. If a higher severity error occurs as part of the batch then prior output is included in the Exception instead.
+
+.PARAMETER FireInfoMessageEventOnUserErrors
+If specified, includes higher Severity information in the Message, up to Severity 16. This means however that no Exception is thrown and the batch will continue to run.
+
+.PARAMETER CommandType
+Text, StoredProcedure, or TableDirect.
+
+.PARAMETER Transaction
+An existing System.Data.SqlClient.Transaction object.
 
 .INPUTS
 Pipe in a connection string, or an System.Data.SqlClient.SqlConnection object.
@@ -14,12 +39,49 @@ Pipe in a connection string, or an System.Data.SqlClient.SqlConnection object.
 A System.Data.SqlClient.SqlCommand object.
 
 .EXAMPLE
-$sqlCommand = New-SqlConnectionString -ServerInstance .\SQL2014 -Database master | New-SqlCommand "Select * From sys.databases Where name = @DatabaseName" `@DatabaseName master -CommandTimeout 10
+Preparing a connection and command with some parameters.
+
+Import-Module SqlHelper
+$sql = New-SqlConnectionString -ServerInstance .\SQL2014 -Database master | New-SqlCommand "Select * From sys.databases Where name = @DatabaseName" `@DatabaseName master -QueryTimeout 10
+
+.EXAMPLE
+Showing how Message works to populate a variable.
+
+Import-Module SqlHelper -Force
+$Error.Clear()
+if (Test-Path Variable:Global:Message) {
+    Remove-Variable Message
+}
+$sql = New-SqlConnectionString .\SQL2014 -Database master | New-SqlCommand "Print 'Hi'" -Message Message
+$sql.Connection.Open()
+$sql.ExecuteScalar()
+$sql.ExecuteScalar()
+$Message
+
+.EXAMPLE
+Demonstrate the various Exception handling.
+
+Import-Module SqlHelper -Force
+$sql = New-SqlConnectionString .\SQL2014 -Database master | New-SqlCommand "Raiserror('Hi', 10, 1)" # No Exception
+$sql.Connection.Open()
+$sql.ExecuteNonQuery()  
+$sql = New-SqlConnectionString .\SQL2014 -Database master | New-SqlCommand "Raiserror('Hi', 11, 1)" # Exception
+$sql.Connection.Open()
+$sql.ExecuteNonQuery()  
+$sql = New-SqlConnectionString .\SQL2014 -Database master | New-SqlCommand "Raiserror('Hi', 16, 1)" -FireInfoMessageEventOnUserErrors # No Exception
+$sql.Connection.Open()
+$sql.ExecuteNonQuery()  
+$sql = New-SqlConnectionString .\SQL2014 -Database master | New-SqlCommand "Raiserror('Hi', 17, 1)" -FireInfoMessageEventOnUserErrors # Exception
+$sql.Connection.Open()
+$sql.ExecuteNonQuery()  
+
+.NOTES
+Verbose output is supported automatically if $VerbosePreference is set at the time of executing the SQL queries, for Severity 1-10 messages.
 
 #>
 
 function New-SqlCommand {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName = "All")]
     param (
         [Parameter(Mandatory = $true, Position = 0)]
         $Query,
@@ -29,9 +91,12 @@ function New-SqlCommand {
         [Parameter(ValueFromPipeline = $true)]
         $Connection, # string or System.Data.SqlClient.SqlConnection
 
-        [int] $CommandTimeout,
-        [string] $InfoMessageVariable,
+        [int] $QueryTimeout,
+        [string] $Message,
         [System.Data.CommandType] $CommandType = "Text",
+        [switch] $FireInfoMessageEventOnUserErrors,
+
+        [Parameter(ParameterSetName = "ExistingTransaction")]
         [System.Data.SqlClient.SqlTransaction] $Transaction
     )
 
@@ -47,48 +112,82 @@ function New-SqlCommand {
         # If neither a connection or connection string were specified then no connection is attached
         $sqlCommand = New-Object System.Data.SqlClient.SqlCommand($Query, $Connection)
         $sqlCommand.CommandType = $CommandType
-        if ($CommandTimeout) {
-            $sqlCommand.CommandTimeout = $CommandTimeout
+        if ($QueryTimeout) {
+            $sqlCommand.CommandTimeout = $QueryTimeout
         }
         if ($Transaction) {
             $sqlCommand.Transaction = $Transaction
         }
- 
-        # Check the various error conditions for parameters that are passed in.
-        if (($Parameters.Count % 2) -ne 0) {
-            Write-Error "Parameters must be passed in pairs in the pattern of @VariableName and Value. A parameter count of $($Parameters.Count) is incorrect."
-        } 
-        $badParameters = @(0..($Parameters.Count - 1)) | Where { ($_ % 2) -eq 0 } | Where { $Parameters[$_] -isnot [string] -or $Parameters[$_] -notlike "@*" } | %{ $_ + 1 }
-        if ($badParameters) {
-            Write-Error "Parameters must be passed in pairs in the pattern of @VariableName and Value. Parameters at these indexes are incorrect: $($badParameters)."            
+        if ($FireInfoMessageEventOnUserErrors) {
+            $sqlCommand.Connection.FireInfoMessageEventOnUserErrors = $FireInfoMessageEventOnUserErrors
         }
 
-        # Build the parameter set
-        $parameterIndex = 0
-        while (($parameterIndex + 1) -lt $Parameters.Count) {
-            $parameter = New-Object System.Data.SqlClient.SqlParameter($Parameters[$parameterIndex++], $Parameters[$parameterIndex++])
-            [void] $sqlCommand.Parameters.Add($parameter)
+        if ($Parameters) {
+            # Check the various error conditions for parameters that are passed in.
+            if (($Parameters.Count % 2) -ne 0) {
+                Write-Error "Parameters must be passed in pairs in the pattern of @VariableName and Value. A parameter count of $($Parameters.Count) is incorrect."
+            } 
+            $badParameters = @(0..($Parameters.Count - 1)) | Where { ($_ % 2) -eq 0 } | Where { $Parameters[$_] -isnot [string] -or $Parameters[$_] -notlike "@*" } | %{ $_ + 1 }
+            if ($badParameters) {
+                Write-Error "Parameters must be passed in pairs in the pattern of @VariableName and Value. Parameters at these indexes are incorrect: $($badParameters)."            
+            }
+
+            # Build the parameter set
+            $parameterIndex = 0
+            while (($parameterIndex + 1) -lt $Parameters.Count) {
+                $parameter = New-Object System.Data.SqlClient.SqlParameter($Parameters[$parameterIndex++], $Parameters[$parameterIndex++])
+                [void] $sqlCommand.Parameters.Add($parameter)
+            }
         }
 
-        # Optionally we attach a script which will write info messages to a variable    
-        if ($InfoMessageVariable) {
-            $infoMessageScriptBlock = @"
-Param (`$sender, `$event)
+        $script1 = @'
+[CmdletBinding()]
+param (
+    $sender,
+    $event
+)
 Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
 
-`$infoMessageVariable = [ref] @() # Populate an empty array, to discard later, if we found no proper variable
-if (Test-Path variable:global:$InfoMessageVariable) {
-    `$infoMessageVariable = Get-Variable -Scope "Global" -Name $InfoMessageVariable -ErrorAction:Stop
+'@
+
+        $script2 = @'
+if (Test-Path variable:global:VariableName) {
+    $messageVariable = Get-Variable -Scope "Global" -Name VariableName
+} else {
+    $messageVariable = New-Variable -Scope "Global" -Name VariableName -Value @() -Passthru
 }
 
-`$event.Errors | %{
-    `$infoMessageVariable.Value += `$_
-    "Msg {0}, Level {1}, State {2}, Line {3}`n{4}" -f `$_.Number, `$_.Class, `$_.State, `$_.LineNumber, `$_.Message | Write-Debug
+$event.Errors | %{
+    try {
+        $messageVariable.Value += $_
+    } catch {
+        $_
+    }
 }
-"@
-            $sqlConnection.add_InfoMessage([ScriptBlock]::Create($infoMessageScriptBlock))        
+
+'@
+
+        $script3 = @'
+$event.Errors | %{
+    if ($_.Class -le 10) {
+        "Msg {0}, Level {1}, State {2}, Line {3}$([Environment]::NewLine){4}" -f $_.Number, $_.Class, $_.State, $_.LineNumber, $_.Message | Write-Verbose
+    } else {
+        # Should be Write-Error but it doesn't seem to trigger properly (after -FireInfoMessageEventOnUserErrors) and so it would otherwise up getting lost
+        "Msg {0}, Level {1}, State {2}, Line {3}$([Environment]::NewLine){4}" -f $_.Number, $_.Class, $_.State, $_.LineNumber, $_.Message | Write-Verbose
+    }
+}
+'@
+
+        if ($Message) {
+            $messageScript = (@($script1, $script2, $script3) -join [Environment]::Newline) -replace "VariableName", $Message
+        } else {
+            $messageScript = (@($script1, $script3) -join [Environment]::Newline)
         }
-  
+        Write-Debug $messageScript
+
+        $Connection.add_InfoMessage([ScriptBlock]::Create($messageScript))        
+
         # Return the command
         $sqlCommand
     }
---
+++ Start-SqlTransaction.ps1
@@ -0,0 +1,61 @@
+﻿<#
+
+.SYNOPSIS
+Starts an SQL Transaction.
+
+.DESCRIPTION
+Starts an SQL Transaction.
+
+.PARAMETER SqlCommand.
+An SqlCommand with an SqlConnection.
+
+.PARAMETER TransactionName
+An optional name for the transaction.
+
+.INPUTS
+Pipe in an SqlCommand.
+
+.OUTPUTS
+The same as was piped in.
+
+.EXAMPLE
+Import-Module SqlHelper
+$sql = New-SqlConnectionString -ServerInstance .\SQL2014 -Database master | New-SqlCommand "Select @@Trancount" | Start-SqlTransaction "ABC"
+$sql.ExecuteScalar()
+
+#>
+
+function Start-SqlTransaction {
+    [CmdletBinding(DefaultParameterSetName = "All")]
+    param (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [System.Data.SqlClient.SqlCommand] $SqlCommand,
+        [Parameter(Position = 0)]
+        [string] $TransactionName,
+        [System.Data.IsolationLevel] $IsolationLevel
+    )
+
+    Begin {
+    }
+
+    Process {
+                if ($SqlCommand.Connection -eq $null) {
+                    Write-Error "SqlCommand requires a valid associated SqlConnection before a transaction can be started."
+                }  if ($SqlCommand.Connection.State -ne "Open") {
+            Write-Verbose "Opening connection"
+            $SqlCommand.Connection.Open()
+        }
+
+        if ($IsolationLevel) {
+            $SqlCommand.Transaction = $SqlCommand.Connection.BeginTransaction($IsolationLevel, $TransactionName)
+        } else {
+            $SqlCommand.Transaction = $SqlCommand.Connection.BeginTransaction($TransactionName)
+        }
+
+        # Pass on the object
+        $SqlCommand
+    }
+
+    End {
+    }
+}
